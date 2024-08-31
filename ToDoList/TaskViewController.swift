0d969caee3c11b35afe2c951820aa94eb46ebd1d
enigmak9 <enigmak9@protonmail.com>
2024-08-31 08:59:51 -0600

    updated code

--- ToDoList/TaskViewController.swift
+++ ToDoList/TaskViewController.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 class TaskViewController: UIViewController {
-
+    
+    @IBOutlet var label: UILabel!
+    
+    var task: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.text = task
 
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
     }
-
+    
+    @objc func deleteTask() {
+        //guard let count = UserDefaults.standard.value(forKey: "count") as? Int, count > 0, let index = taskIndex else {
+        //    return
+        //}
+        
+        // let newCount = count - 1
+        
+        // UserDefaults.standard.setValue(newCount, forKey: "count")
+        // UserDefaults.standard.setValue(nil, forKey: "task_\(currentPosition)")
+        
+        // navigationController?.popViewController(animated: true) // Go back to the previous screen after deletion
+    }
 }
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -9,39 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var TableView: UITableView! // nos permite controlar el tableview en el storyboard
-    var tasks = [String]() // Corrected type and added initialization
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
+    var tasks = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Tasks"
         
-        TableView.delegate = self
+        tableView.delegate = self
         tableView.dataSource = self
-        
-        //setup
-        
-        if  !UserDefaults().bool(forKey: "setup") {
-            UserDefaults().set(true, forKey: "setup")
-            UserDefaults().set(0, forKey: "count")
-            
+        // Initial setup
+        if !UserDefaults.standard.bool(forKey: "setup") {
+            UserDefaults.standard.set(true, forKey: "setup")
+            UserDefaults.standard.set(0, forKey: "count")
         }
         
-        // Do any additional setup after loading the view.
-        // obtener todos los datos salvados
+        updateTasks()
+    }
+    
+    
+    func updateTasks() {
+        tasks.removeAll()
+        
+        guard let count = UserDefaults.standard.value(forKey: "count") as? Int else {
+            return
+        }
+        
+        for x in 0..<count {
+            if let task = UserDefaults.standard.string(forKey: "task_\(x+1)") {
+                tasks.append(task)
+            }
+        }
+        
+        tableView.reloadData()
     }
     
     @IBAction func didTapAdd() {
-        let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
-        vc.title = "Nueva tarea"
+        let vc = storyboard?.instantiateViewController(withIdentifier: "task") as! EntryViewController
+        vc.title = "New Task"
+        vc.update = {
+            DispatchQueue.main.async {
+                self.updateTasks()
+            }
+        }
         navigationController?.pushViewController(vc, animated: true)
     }
+
 }
 
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true) // Corrected the argument to lowercase indexPath
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = storyboard?.instantiateViewController(withIdentifier: "task") as! TaskViewController
+        vc.title = "New Task"
+        vc.task = tasks[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
